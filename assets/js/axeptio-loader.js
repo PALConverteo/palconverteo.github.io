514f1c2ddfccdfcf0d4846bfd1c4c58f4186a4e2
Pierre-Adrien Lair <pala@converteo.local>
2025-06-20 14:47:28 +0200

    refactor: centralize Axeptio CMP script

---
+++ assets/js/axeptio-loader.js
@@ -0,0 +1,19 @@
+window.axeptioSettings = {
+  clientId: "685553b10ecc9f1c8a6c31f4",
+  cookiesVersion: "palconverteo-fr-EU",
+  googleConsentMode: {
+    default: {
+      analytics_storage: "denied",
+      ad_storage: "denied",
+      ad_user_data: "denied",
+      ad_personalization: "denied",
+      wait_for_update: 500
+    }
+  }
+};
+
+(function(d, s) {
+  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  e.async = true; e.src = "//static.axept.io/sdk.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script"); 
